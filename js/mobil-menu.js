40deca0bb03044fb30f51c2795bc9eb074ebe072
OksanaBuchkovska <107949958+OksanaBuchkovska@users.noreply.github.com>
2022-09-01 12:46:40 +0300

    респонсив герой

--- js/mobil-menu.js
+++ js/mobil-menu.js
@@ -3,12 +3,21 @@
     openMenuBtn: document.querySelector(".menu-open"),
     closeMenuBtn: document.querySelector(".menu-close"),
     menu: document.querySelector(".menu"),
+    body: document.querySelector("body"),
+    menuList: document.querySelector(".menu-list"),
   };
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
   refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  refs.menuList.addEventListener("click", removeMenu);
 
   function toggleMenu() {
     refs.menu.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
+    refs.body.classList.toggle("no-scroll");
+  }
+
+  function removeMenu() {
+    refs.menu.classList.add("is-hidden");
   }
 })();
